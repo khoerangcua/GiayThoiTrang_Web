2d276dfdfe8b403da21c62dfd5dd4d2bcd56e5b0
Bảo Đặng <baodang.31191023095@st.ueh.edu.vn>
2021-10-12 22:34:40 +0700

    Bao - Sua script back to top

--- public/scripts/footer.js
+++ public/scripts/footer.js
@@ -1,7 +1,6 @@
 //Get the button:
 mybutton = document.getElementById("back-to-top");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -13,11 +12,7 @@ function scrollFunction() {
   
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-
-
-
+function scrollToTop() {
+  
+  window.scrollTo(0, 0);
 }
